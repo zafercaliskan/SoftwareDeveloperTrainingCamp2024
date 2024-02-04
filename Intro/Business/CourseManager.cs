0a9008ef5b5c730f47ca3e8254d22d4a02bcfdbf
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2024-02-04 23:04:23 +0300

    first day object oriented programming (opp)

---
+++ Intro/Business/CourseManager.cs
@@ -0,0 +1,40 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business
+{
+
+    public class CourseManager
+    {
+        Course[] courses = new Course[3];
+
+        //constructor
+        public CourseManager()
+        {
+            Course course1 = new Course();
+            course1.Id = 1;
+            course1.Name = "C#";
+
+            Course course2 = new Course();
+            course2.Id = 2;
+            course2.Name = "Java";
+
+            Course course3 = new Course();
+            course3.Id = 3;
+            course3.Name = "Python";
+
+            courses[0] = course1;
+            courses[1] = course2;
+            courses[2] = course3;   
+        }
+
+        public Course[] GetAll()
+        {
+            return courses;
+        }
+    }
+}
--- Intro/Program.cs
+++ Intro/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Intro.Business;
+using Intro.Entities;
+
 Console.WriteLine("Hello, World!");
 
 //Daha farklı örnekler burda https://www.w3schools.com/cs/cs_intro.php 
@@ -47,4 +50,33 @@ string[] loans = { "Kredi1", "Kredi2", "Kredi3", "Kredi4", "Kredi5" };
 for (int i = 0; i < loans.Length; i++)
 {
     Console.WriteLine(loans[i]);
+}
+
+
+Course course1 = new Course();
+course1.Id = 1;
+course1.Name = "C#";
+
+Course course2 = new Course();
+course2.Id = 2;
+course2.Name = "Java";
+
+Course course3 = new Course();
+course3.Id = 3;
+course3.Name = "Python";
+
+Course[] courses = { course1, course2, course3 };
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Id + " / " + courses[i].Name);
+}
+
+
+CourseManager courseManager = new();
+Course[] courses2 = courseManager.GetAll();
+
+
+foreach (var course in courses2)
+{
+    Console.WriteLine(course.Id + " / " + course.Name);
 }
